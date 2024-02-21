31801769ea9498bb05f3a667957cb6e735baae95
LucaTosetti01 <luca1.tosetti@mail.polimi.it>
2024-02-21 15:46:55 +0100

    Worker main WIP

--- src/main/java/it/polimi/ds/map_reduce/socket/packets/ScheduleJobPacket.java
+++ src/main/java/it/polimi/ds/map_reduce/socket/packets/ScheduleJobPacket.java
@@ -1,8 +1,9 @@
 package it.polimi.ds.map_reduce.socket.packets;
 
+import it.polimi.ds.map_reduce.Tuple2;
 import it.polimi.ds.map_reduce.js.Op;
 
 import java.util.List;
 
-public record ScheduleJobPacket(int jobId, List<Op> ops) implements S2CPacket {
+public record ScheduleJobPacket(int jobId, List<Op> ops, List<Tuple2> data) implements S2CPacket {
 }
---
+++ src/main/java/it/polimi/ds/map_reduce/worker/WorkerMain.java
@@ -0,0 +1,41 @@
+package it.polimi.ds.map_reduce.worker;
+
+import it.polimi.ds.map_reduce.Tuple2;
+import it.polimi.ds.map_reduce.js.CompiledOp;
+import it.polimi.ds.map_reduce.js.CompiledProgram;
+import it.polimi.ds.map_reduce.js.Program;
+import it.polimi.ds.map_reduce.socket.packets.JobResultPacket;
+import it.polimi.ds.map_reduce.socket.packets.ScheduleJobPacket;
+import org.openjdk.nashorn.api.scripting.NashornScriptEngineFactory;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptException;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.List;
+
+public class WorkerMain {
+
+    public static void main(String[] args) throws IOException {
+        ScriptEngine engine = new NashornScriptEngineFactory().getScriptEngine("--language=es6", "-doe");
+
+        try (WorkerSocketManager mngr = new WorkerSocketManagerImpl(new Socket("127.0.0.1", 6666))) {
+
+            while (!Thread.interrupted()) {
+                var ctx = mngr.receive(ScheduleJobPacket.class);
+
+                new Thread(() -> {
+                    ScheduleJobPacket job = ctx.getPacket();
+
+                    try {
+                        List<CompiledOp> compileOps = Program.compile(engine, job.ops());
+                        List<Tuple2> res = CompiledProgram.execute(compileOps, job.data().stream()).toList();
+                        ctx.reply(new JobResultPacket(res));
+                    } catch (IOException | ScriptException e) {
+                        throw new RuntimeException(e);
+                    }
+                }).start();
+            }
+        }
+    }
+}
