f8b13c093eb590bfcfa5325cd3cde6c9eb3afe45
Furrrlo <ferloo@protonmail.com>
2024-02-22 15:37:10 +0100

    Add NullMarked to all packages

---
+++ src/main/java/it/polimi/ds/map_reduce/js/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package it.polimi.ds.map_reduce.js;
+
+import org.jspecify.annotations.NullMarked;
---
+++ src/main/java/it/polimi/ds/map_reduce/utils/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package it.polimi.ds.map_reduce.utils;
+
+import org.jspecify.annotations.NullMarked;
