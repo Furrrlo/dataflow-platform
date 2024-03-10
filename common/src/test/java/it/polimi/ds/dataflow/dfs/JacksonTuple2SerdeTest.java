ec2b70383d251fbc1011d477826e6f2d71f296b7
LucaTosetti01 <luca1.tosetti@mail.polimi.it>
2024-03-10 18:08:35 +0100

    Serde asserts WIP

--- common/src/test/java/it/polimi/ds/dataflow/dfs/JacksonTuple2SerdeTest.java
+++ common/src/test/java/it/polimi/ds/dataflow/dfs/JacksonTuple2SerdeTest.java
@@ -2,6 +2,7 @@ package it.polimi.ds.dataflow.dfs;
 
 import it.polimi.ds.dataflow.Tuple2;
 import org.junit.jupiter.api.Test;
+import org.openjdk.nashorn.api.scripting.JSObject;
 import org.openjdk.nashorn.api.scripting.NashornScriptEngineFactory;
 import org.openjdk.nashorn.api.scripting.ScriptObjectMirror;
 
@@ -9,6 +10,7 @@ import javax.script.ScriptEngine;
 import javax.script.ScriptException;
 import java.util.List;
 import java.util.Objects;
+import java.util.function.Function;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -16,6 +18,7 @@ class JacksonTuple2SerdeTest {
 
     static final ScriptEngine ENGINE = new NashornScriptEngineFactory().getScriptEngine("--language=es6", "-doe");
     static final JacksonTuple2Serde SERDE;
+
     static {
         try {
             SERDE = new JacksonTuple2Serde(ENGINE);
@@ -53,16 +56,43 @@ class JacksonTuple2SerdeTest {
     }
 
     @Test
+    @SuppressWarnings("unchecked")
     void serdeMap() throws ScriptException {
         var arr = ENGINE.eval("new Map([ [1, 2], ['ciao', 4] ])");
         var tuple = new Tuple2(arr, arr);
         var json = SERDE.jsonify(tuple);
         var res = SERDE.parseJson(json);
         System.out.println(json);
-        // TODO: assert
+
+        var mapToArrayFnObj = ((ScriptObjectMirror) arr).eval("""
+                (function(map) {
+                    let arr = [];
+                    for(const e of map.entries()) { arr.push(e); }
+                    return arr;
+                })
+                """);
+
+        Function<Object, Object> mapToArrayFn = mapToArrayFnObj instanceof ScriptObjectMirror mirror
+                ? mirror.to(Function.class)
+                : m -> ((JSObject) mapToArrayFnObj).call(null, m);
+
+//        var printFnObj = ((ScriptObjectMirror) arr).eval("(function(arr) {print(arr)})");
+//        Function<Object,Object> printFn = printFnObj instanceof ScriptObjectMirror mirror ? mirror.to(Function.class) : null;
+//        System.out.println(printFn.apply(mapToArrayFn.apply(res.key())));
+
+        assertEquals(
+                ((ScriptObjectMirror) mapToArrayFn.apply(arr)).to(List.class),
+                //TODO: The upper one works as intended, the bottom one no, HOW?
+                ((ScriptObjectMirror) Objects.requireNonNull(mapToArrayFn.apply(res.key()))).to(List.class)
+        );
+        assertEquals(
+                ((ScriptObjectMirror) mapToArrayFn.apply(arr)).to(List.class),
+                ((ScriptObjectMirror) Objects.requireNonNull(mapToArrayFn.apply(res.value()))).to(List.class)
+        );
     }
 
     @Test
+    @SuppressWarnings("unchecked")
     void serdeSet() throws ScriptException {
         var arr = ENGINE.eval("new Set([ 1, 2, 3, 4, 5 ])");
         var tuple = new Tuple2(arr, arr);
@@ -70,6 +100,25 @@ class JacksonTuple2SerdeTest {
         var res = SERDE.parseJson(json);
         System.out.println(json);
         // TODO: assert
+        var setToArrayFnObj = ((ScriptObjectMirror) arr).eval("""
+                (function(set) {
+                    let arr = [];
+                    for(const e of set.values()) { arr.push(e); }
+                    return arr;
+                })
+                """);
+        Function<Object, Object> setToArrayFn = setToArrayFnObj instanceof ScriptObjectMirror mirror
+                ? mirror.to(Function.class)
+                : m -> ((JSObject) setToArrayFnObj).call(null, m);
+
+        assertEquals(
+                ((ScriptObjectMirror) setToArrayFn.apply(arr)).to(List.class),
+                ((ScriptObjectMirror) Objects.requireNonNull(setToArrayFn.apply(res.key()))).to(List.class)
+        );
+        assertEquals(
+                ((ScriptObjectMirror) setToArrayFn.apply(arr)).to(List.class),
+                ((ScriptObjectMirror) Objects.requireNonNull(setToArrayFn.apply(res.value()))).to(List.class)
+        );
     }
 
     @Test
