95ba290d1a48ed13067f7f334e16f5237681cdc8
Furrrlo <ferloo@protonmail.com>
2024-03-12 15:02:52 +0100

    Cleanup integration-tests build file

--- integration-tests/build.gradle.kts
+++ integration-tests/build.gradle.kts
@@ -3,10 +3,18 @@ plugins {
 }
 
 dependencies {
-    testImplementation(projects.coordinator)
-    testImplementation(projects.worker)
-    testImplementation(projects.common)
-    testImplementation(testFixtures(projects.common))
+    listOf(
+        projects.coordinator,
+        projects.worker
+    ).forEach {
+        testImplementation(it) {
+            attributes {
+                // Also request the runtime dependencies (the ones declared as implementation)
+                attribute(Usage.USAGE_ATTRIBUTE, objects.named(Usage::class.java, Usage.JAVA_RUNTIME))
+            }
+        }
+    }
 
+    testImplementation(testFixtures(projects.common))
     testImplementation(libs.slf4j.simple)
 }
