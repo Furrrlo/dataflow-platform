3cd77ff67e586279b5c33d52b49c4bccd446eadc
Furrrlo <ferloo@protonmail.com>
2024-04-18 20:24:51 +0200

    Split isTerminal and isRequiresShuffling flags in OpKind

--- common/src/main/java/it/polimi/ds/dataflow/js/OpKind.java
+++ common/src/main/java/it/polimi/ds/dataflow/js/OpKind.java
@@ -5,26 +5,28 @@ import org.jetbrains.annotations.Unmodifiable;
 import java.util.List;
 
 public enum OpKind {
-    FLAT_MAP("flatMap", true, false),
+    FLAT_MAP("flatMap", OpKind.SHUFFLES),
     MAP("map"),
     FILTER("filter"),
-    CHANGE_KEY("changeKey", true, false),
+    CHANGE_KEY("changeKey", OpKind.SHUFFLES),
     REDUCE("reduce");
 
     public static final @Unmodifiable List<OpKind> VALUES = List.of(values());
 
-    private final String name;
-    private final boolean shuffles;
-    private final boolean isTerminal;
+    private static final int SHUFFLES = 0x1;
+    private static final int REQUIRES_SHUFFLE = 0x2;
+    private static final int TERMINAL = 0x4;
 
-    OpKind(String name, boolean shuffles, boolean isTerminal) {
+    private final String name;
+    private final int flags;
+
+    OpKind(String name, int flags) {
         this.name = name;
-        this.shuffles = shuffles;
-        this.isTerminal = isTerminal;
+        this.flags = flags;
     }
 
     OpKind(String name) {
-        this(name, false, false);
+        this(name, 0);
     }
 
     public String getName() {
@@ -32,22 +34,24 @@ public enum OpKind {
     }
 
     public boolean isTerminal() {
-        return isTerminal;
+        return (flags & TERMINAL) != 0;
     }
 
     public boolean isShuffles() {
-        return shuffles;
+        return (flags & SHUFFLES) != 0;
     }
 
     public boolean isRequiresShuffling() {
-        return isTerminal;
+        return (flags & REQUIRES_SHUFFLE) != 0;
     }
 
     @Override
     public String toString() {
         return "OpKind{" +
                 "name='" + name + '\'' +
-                ", isTerminal=" + isTerminal +
+                ", isTerminal=" + isTerminal() +
+                ", isShuffles=" + isShuffles() +
+                ", isRequiresShuffling=" + isRequiresShuffling() +
                 "}";
     }
 }
